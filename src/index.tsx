30aa023118e23c0daa80adad39ef78385082fa10
Railly Koeiman <railly6@live.nl>
2020-02-24 22:02:44 +0100

    Optimized bundle size and scss file structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './js/App';
 
-render(<App />, document.getElementById('root'));
+import Loadable from 'react-loadable';
+import Loading from './js/pages/Loading';
+
+const LoadPage = Loadable({
+	loader: () => import('./js/App'),
+	loading: (props) => <Loading {...props} />,
+	timeout: 5000
+});
+
+render(<LoadPage />, document.getElementById('root'));
