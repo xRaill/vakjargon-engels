e4cb9b80c812541d720d465f97eb8374a996bf1d
root <root@DESKTOP-IEOFQJ1.localdomain>
2020-04-08 09:11:20 +0200

    Seperated buttons

--- src/js/pages/Game.tsx
+++ src/js/pages/Game.tsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useRef, useCallback, useReducer } from 'react';
 import { Redirect } from 'react-router-dom';
-import Transition from '../components/Transition';
 import Score from '../components/Quiz/Score';
 import Title from '../components/Quiz/Title';
+import Button from '../components/Quiz/Button';
 
 import styles from '../../scss/pages/Game.scss';
 
@@ -13,106 +13,67 @@ interface Quiz  {
 	correct: number
 }
 
-const Questions: Quiz[] = require('../quiz.json');
+const Data = require('../quiz.json');
 
-let Quiz: Quiz[] = Questions.slice();
+const quizManager = (quiz: {current: any, data: Quiz[]}, newQuestion) => {
+	const randomNum = ~~(Math.random() * quiz.data.length);
+	return {
+		current: quiz.data[randomNum],
+		data: quiz.data.filter((v,i) => i !== randomNum)
+	};
+}
 
 const Game = () => {
 
-	const [currentQuestion, setCurrentQuestion] = useState<Quiz>();
-	const [chosenAnswer,setChosenAnswer] = useState<number>();
-	const [answers, setAnswers] = useState<{i: number; v: string, b: string}[]>();
-	const [visible, setVisible] = useState<boolean>(false);
-	const [disabled, setDisabled] = useState<boolean>(true);
-	const [questionI, setQuestionI] = useState<number>(0);
+	const [Quiz, newQuestion] = useReducer(quizManager, quizManager({current: {}, data: Data.concat()}, true));
+	const [buttons, setButtons] = useState<boolean>(true);
+	const [disabled, setDisabled] = useState<boolean>(false)
 	const [redirect, setRedirect] = useState<boolean>(false);
 	const scoreRef = useRef(null);
-
-	useEffect(() => {
-
-		const randomQuestion = () => {
-			const randomNum: number = Math.floor(Math.random() * Quiz.length);
-			
-			setCurrentQuestion(Quiz[randomNum]);
-			
-			const unsortedAnswers = Quiz[randomNum].options.map((a,i) => ({i:i,a:a}));
-			let sortedAnswers = [];
-			for (let i = 0; i < 4; i++) {
-				let num = [[0,1,2,3],[3,2,0,1],[1,0,3,2],[2,3,1,0]][questionI][i];
-
-				sortedAnswers.push({
-					i: unsortedAnswers[num].i,
-					v: unsortedAnswers[num].a,
-					b: 'primary'
-				});
-			}
-			setQuestionI(questionI == 3 ? 0 : questionI + 1);
-			
-			setAnswers(sortedAnswers);
-			setVisible(true);
-			setDisabled(false);
-		}
-		
-		if(!currentQuestion) randomQuestion();
-		
-		if(typeof(chosenAnswer) === 'number') {
-			const answer = answers.find(a => a.i == chosenAnswer);
-
-			setDisabled(true);
-
-			if(chosenAnswer === currentQuestion.correct) {
-				answer.b = 'success';
-				scoreRef.current.increaseScore();
-			} else {
-				answer.b = 'danger';
-				scoreRef.current.decreaseScore();
-			}
-			
-			setTimeout(() => {
-				setVisible(false);
-				Quiz.splice(Quiz.findIndex(a => a.id === currentQuestion.id), 1);
-				if(Quiz.length) setTimeout(() => randomQuestion(), 500);
-				else setTimeout(() => setRedirect(true), 500);
-			}, 1000);
-		}
-	}, [chosenAnswer]);
 	
-	if(redirect) {
-		Quiz = Questions.slice();
-		return <Redirect push to={{
-			pathname: 'complete',
-			state: {score: scoreRef.current.getScore()}
-		}}/>
-	}
+	const handleAnswer = useCallback(answer => {
+		setDisabled(true);
+		setTimeout(() => {
+			setButtons(false);
+
+			if(Quiz.data.length) setTimeout(() => {
+				newQuestion(null);
+				setButtons(true);
+				setDisabled(false);
+			}, 500);
+			else setRedirect(true);
+		}, 700);
+
+		if(Quiz.current.options[Quiz.current.correct] === answer) {
+			scoreRef.current.increaseScore();
+			return 'success';
+		} else {
+			scoreRef.current.decreaseScore();
+			return 'danger';
+		}
+	}, [Quiz]);
+
+	if(redirect) return <Redirect push to={{
+		pathname: 'complete',
+		state: {score: scoreRef.current.getScore()}
+	}}/>
 	
 	return(
-		<>
-		<div className={`${styles.row} ${styles["py-lg-5"]}`}>
-			<div className={styles["col-2"]}>
-				<Score ref={scoreRef} />
+		<div className={styles.container}>
+			<div className={`${styles.row} ${styles["py-lg-5"]}`}>
+				<div className={styles["col-2"]}>
+					<Score ref={scoreRef} />
+				</div>
+				<div className={styles["col-8"]}>
+					<Title>{Quiz.current.question}</Title>
+				</div>
 			</div>
-			<div className={styles["col-8"]}>
-				<Title>{currentQuestion ? currentQuestion.question : ''}</Title>
+			<div className={styles.row}>
+				{Quiz.current.options.map((v: string, i: number) =>
+					<Button key={i} hidden={!buttons} disabled={disabled} callback={handleAnswer}>{v}</Button>
+				)}
 			</div>
 		</div>
-		<Transition in={visible} timeout={500} className={"animateButtons"} style={styles}>
-			<div className={`${styles.row}`}>
-				{currentQuestion ? answers.map(a =>
-					<div key={a.i} className={`
-						${styles["col-lg-6"]} ${styles.center}
-						${styles["py-4"]} ${styles["py-lg-5"]}
-						${disabled ? styles.disableClick :''}
-					`}>
-						<button onClick={()=> setChosenAnswer(a.i)} className={`
-							${styles.btn} ${styles["btn-"+ a.b]} ${styles["w-75"]} ${styles["py-3"]}
-						`}>
-							{a.v}
-						</button>
-					</div>
-				):''}
-			</div>
-		</Transition>
-		</>
 	);
 };
 
